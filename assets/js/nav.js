edea315aec239c3880e5292b03f013efbfa0cb62
MTSSamsioe <michael.samsioe@gmail.com>
2024-10-11 21:51:31 +0200

    fix realtive file paths

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,6 +1,6 @@
 //Nav load
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('includes/nav.html')
+    fetch('../../includes/nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('nav-placeholder').innerHTML = data;
