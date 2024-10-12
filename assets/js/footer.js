caaf180be384b52e49a9c27ea8c49dffc7698ddf
MTSSamsioe <michael.samsioe@gmail.com>
2024-10-12 12:56:10 +0200

    fix attempt file paths

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,6 +1,6 @@
 //footer load
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('../../includes/footer.html')
+    fetch('includes/footer.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('footer-placeholder').innerHTML = data;
